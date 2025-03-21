218feb4f15de1c18b09257987cbc5192a56ddd9a
viddeveloper3 <viddeveloper3@gmail.com>
2025-03-21 18:22:11 +0530

    Update authentication.js

--- Authentication/authentication.js
+++ Authentication/authentication.js
@@ -57,7 +57,6 @@ exports.login = async function (req, res, next) {
     res.cookie("auth", token, {
       httpOnly: false,
       secure: false,
-      domain: 'http://localhost:3001',
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
