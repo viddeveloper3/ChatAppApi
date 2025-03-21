9c2cc5c20e90f72129d9943b83c4da9cebb55919
viddeveloper3 <viddeveloper3@gmail.com>
2025-03-21 22:17:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 // WebSocket Server Setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://quicktalkapp.netlify.app",
     methods: ["GET", "POST", "PATCH", "DELETE"],
   },
 });
